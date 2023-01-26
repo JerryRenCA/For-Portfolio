99a94a5febe25508b4ea64ebe569d66b6bcef042
Jerry Ren <jerryren@Jerrys-MacBook-Pro.local>
2023-01-26 15:43:03 -0400

    revised on Jan 26,2023

--- js/script.js
+++ js/script.js
@@ -158,3 +158,12 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
